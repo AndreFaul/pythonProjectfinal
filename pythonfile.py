da645a9e95af5f97c3a064394db7b10a81b0af1c
Andre Faul <andrefaul@hotmail.co.uk>
2021-03-24 15:31:41 +0000

    Test

--- pythonfile.py
+++ pythonfile.py
@@ -8,5 +8,13 @@ from functools import reduce
 
 
 print(12, 21, 1974, sep ='/')
+print("No Newline", end='')
+print("hello")
+print("\n%04d %s %.2f %c" %(1, "derek", 1.234, 'A'))
 
-print("hello world")
+i1 = 2
+i1 += 1
+print("i1",i1)
+
+print(math.inf > 0)
+print(math.inf -math.inf)
